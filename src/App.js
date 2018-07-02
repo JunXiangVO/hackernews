75eb44f70d4642b75fdf0a4d769b7dd814d9fff4
JunXiangVO <jun.xiang@vistaoutdoor.com>
2018-07-02 15:33:50 -0500

    Paginated Fetch

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 
 import './App.css';
 
-const DEFAULT_QUERY = '';
+const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 //const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
 
   
@@ -29,11 +32,14 @@ class App extends Component {
   }
 
   setSearchTopStories(result) {
-    this.setState({ result });
+    const { hits, page } = result;
+    const oldHits = page !== 0 ? this.state.result.hits : [];
+    const updatedHits = [...oldHits, ...hits];
+    this.setState({result: { hits: updatedHits, page }});
   }
 
-  fetchSearchTopStories(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  fetchSearchTopStories(searchTerm, page = 0) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
     .then(response => response.json())
     .then(result => this.setSearchTopStories(result))
     .catch(error => error);
@@ -68,6 +74,7 @@ class App extends Component {
 
   render() {
     const{searchTerm, result}=this.state;
+    const page = (result && result.page) || 0;
     if (!result) { return null; }
     return (
       <div className="page">
@@ -75,7 +82,9 @@ class App extends Component {
 
         <Search value={searchTerm} onChange={this.onSearchChange} onSubmit={this.onSearchSubmit}/>
         {result ? <Table list={result.hits} onDismiss={this.onDismiss} /> : null}
-          
+
+        <div className="interactions"> <button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}> More</button></div>
+
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
